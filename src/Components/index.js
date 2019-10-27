575bc8415a75120d1f5facf1c6ed667616413747
nazim-k <kurtametovnazim@gmail.com>
2019-10-27 08:52:22 +0500

    Updates

--- src/Components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './Header';
-export { default as PokemonCard } from './PokemonCard';
-export { default as EvolutionList } from './EvolutionList';
-export { default as ErrorMessage } from './ErrorMessage';
--- src/Containers/index.js
+++
@@ -1,2 +0,0 @@
-export { default as EvolutionListContainer } from './EvolutionListContainer';
-export { default as PokemonContainer } from './PokemonContainer';
